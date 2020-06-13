15a05c4bad28705b1e7014b83223ae811dfcd3c5
Adrian Bolinger <adrian.bolinger@me.com>
2020-06-12 23:55:42 -0400

    Initial Commit

---
+++ ValidationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ValidationDemo
+//
+//  Created by Adrian Bolinger on 6/12/20.
+//  Copyright © 2020 Adrian Bolinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
